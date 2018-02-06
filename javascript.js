2fa3be344009927153dd3f7a82f3c4471c2b6b08
Daniel Griffiths <logiconictvtube@gmail.com>
2018-02-05 21:36:00 -0500

    javascript funcitonality for swithcing displays form itnro to site

--- javascript.js
+++ javascript.js
@@ -1,6 +1,17 @@
 $( document ).ready(function() {
+
   $('.btn.btn-default').click(function() {
       $('.skills').show(500);
-      $('.container-one').hide(500);
+      $('.container-one').hide(0);
   });
+
+  var $slider = document.getElementById('slider');
+  var $toggle = document.getElementById('buttonStart');
+
+  $toggle.addEventListener('click', function() {
+    var isOpen = $slider.classList.contains('slide-in');
+
+    $slider.setAttribute('class', isOpen ? 'slide-out' : 'slide-in');
+  });
+
 });
